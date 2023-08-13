6be0d85217298a7cf5284b73ace329f49f729a31
eplaksina <eplaksina@angelsit.ru>
2023-08-13 22:07:53 +0800

    fix: bag and refactoring

---
+++ js/constans.js
@@ -0,0 +1,6 @@
+export const BOARD_ITEM_ACTIVE = 'tic-tac-toe__board-item_active';
+
+export const HELP_BOARD_ITEM_ACTIVE_HELP = 'tic-tac-toe__for-users-board-item_active-help';
+export const HELP_BOARD_ITEM_ACTIVE = 'tic-tac-toe__for-users-board-item_active';
+
+export const ALL_BOARD_ITEMS_LENGHT = 18;
--- js/game.js
+++ js/game.js
@@ -1,4 +1,5 @@
 import { checkEnd, checkWinVars } from './gameArrays.js'
+import { BOARD_ITEM_ACTIVE, HELP_BOARD_ITEM_ACTIVE_HELP, HELP_BOARD_ITEM_ACTIVE,  ALL_BOARD_ITEMS_LENGHT} from './constans.js'
 
 const { user1, user2, whoStart } = sessionStorage;
 const forUsers = document.getElementsByClassName('tic-tac-toe__for-users-text')[0]
@@ -17,35 +18,34 @@ let xOrO = 'o'
 
 for (let i = 0; i < boardItems.length; i++) {
   boardItems[i].addEventListener('click', () => {
-    deleteHelpMove(xOrO)
+    if (getDataActive(boardItems[i]) === '') {
+      deleteHelpMove(xOrO)
 
-    boardItems[i].classList.add(`tic-tac-toe__board-item_active-${xOrO}`);
-    boardItems[i].setAttribute("data-active", `${stage % 2 ? 'o' : 'x'}`);
+      boardItems[i].classList.add(`${BOARD_ITEM_ACTIVE}-${xOrO}`);
+      boardItems[i].setAttribute("data-active", `${stage % 2 ? 'o' : 'x'}`);
 
-    helpBoardItems[i].classList.add(`tic-tac-toe__for-users-board-item_active-${xOrO}`);
-    helpBoardItems[i].setAttribute("data-active", `${stage % 2 ? 'o' : 'x'}`);
+      helpBoardItems[i].classList.add(`${HELP_BOARD_ITEM_ACTIVE}-${xOrO}`);
+      helpBoardItems[i].setAttribute("data-active", `${stage % 2 ? 'o' : 'x'}`);
 
 
-    if (isGameEnd(xOrO)) {
-      popupTitle.innerHTML = `Пользователь с именем ${whoMove} выиграл! Поздравляем!`
-      popup.classList.remove('hidden')
-    } else if (isNobodyWin()) {
-      popupTitle.innerHTML = `Ничья!`
-      popup.classList.remove('hidden')
-    } else {
-      stage++;
-      xOrO = stage % 2 ? 'o' : 'x';
-
-      if (whoStart === user1) {
-        whoMove = stage % 2 === 0 ? user2 : user1
+      if (isGameEnd(xOrO)) {
+        poopupVisible(`Пользователь с именем ${whoMove} выиграл! Поздравляем!`)
+      } else if (isNobodyWin()) {
+        poopupVisible(`Ничья!`)
       } else {
-        whoMove = stage % 2 === 0 ? user1 : user2
+        stage++;
+        xOrO = stage % 2 ? 'o' : 'x';
+
+        if (whoStart === user1) {
+          whoMove = stage % 2 === 0 ? user2 : user1
+        } else {
+          whoMove = stage % 2 === 0 ? user1 : user2
+        }
+
+        forUsers.innerHTML = `Сейчас ходит: ${whoMove}`
+        checkWin()
       }
-
-      forUsers.innerHTML = `Сейчас ходит: ${whoMove}`
-      checkWin(xOrO)
     }
-
   })
 }
 
@@ -53,26 +53,31 @@ for (let i = 0; i < boardItems.length; i++) {
 const isGameEnd = (xOrO) => {
   for (let i = 0; i < checkEnd.length; i++) {
     const row = checkEnd[i]
-    if (getDataActive(boardItems[row[0]]) == xOrO && getDataActive(boardItems[row[1]]) == xOrO && getDataActive(boardItems[row[2]]) == xOrO) {
-      return 1
+    if (getDataActive(boardItems[row[0]]) === xOrO && getDataActive(boardItems[row[1]]) === xOrO && getDataActive(boardItems[row[2]]) === xOrO) {
+      return true
     }
   }
 
-  return 0
+  return false
 }
 
 const isNobodyWin = () => {
   const dataActiveEls = Array.from(document.querySelectorAll('[data-active]'))
-  const isAllBoardItemsFull = dataActiveEls.filter(item => item.getAttribute('data-active') === 'x' || item.getAttribute('data-active') === 'o')
-  return isAllBoardItemsFull.length == 18 ? 1 : 0
+  const isAllBoardItemsFull = dataActiveEls.filter(item => {
+    const attribute = getDataActive(item)
+    return attribute === 'x' || attribute === 'o'
+  })
+  return isAllBoardItemsFull.length === ALL_BOARD_ITEMS_LENGHT
 }
 
-const checkWin = (xOrO) => {
+const checkWin = () => {
   for (let i = 0; i < checkWinVars.length; i++) {
     const row = checkWinVars[i]
+    const attribute1 = getDataActive(helpBoardItems[row[0]]);
+    const attribute2 = getDataActive(helpBoardItems[row[1]]);
     if (
-      ((getDataActive(helpBoardItems[row[0]]) == 'x' && getDataActive(helpBoardItems[row[1]]) == 'x') ||
-      (getDataActive(helpBoardItems[row[0]]) == 'o' && getDataActive(helpBoardItems[row[1]]) == 'o')) && getDataActive(boardItems[row[2]]) === ''
+      ((attribute1 === 'x' && attribute2 === 'x') ||
+        (attribute1 === 'o' && attribute2 === 'o')) && getDataActive(boardItems[row[2]]) === ''
     ) {
       setHelpMove(helpBoardItems[row[2]])
       helpBoardTitle.classList.remove('hidden')
@@ -86,8 +91,8 @@ const getDataActive = (el) => {
 }
 
 const setHelpMove = (el) => {
-  el.classList.add(`tic-tac-toe__for-users-board-item_active-${xOrO}`);
-  el.classList.add(`tic-tac-toe__for-users-board-item_active-help`);
+  el.classList.add(`${HELP_BOARD_ITEM_ACTIVE}-${xOrO}`);
+  el.classList.add(`${HELP_BOARD_ITEM_ACTIVE_HELP}`);
   el.setAttribute("data-activehelp", `1`);
 }
 
@@ -96,9 +101,14 @@ const deleteHelpMove = (xOrO) => {
   helpBoard.classList.add('hidden')
 
   const helpBoardItemsData = document.querySelectorAll('[data-activehelp="1"]')
-  for(let i=0; i<helpBoardItemsData.length;i++){
-    helpBoardItemsData[i].classList.remove(`tic-tac-toe__for-users-board-item_active-${xOrO}`);
-    helpBoardItemsData[i].classList.remove(`tic-tac-toe__for-users-board-item_active-help`);
+  for (let i = 0; i < helpBoardItemsData.length; i++) {
+    helpBoardItemsData[i].classList.remove(`${HELP_BOARD_ITEM_ACTIVE}-${xOrO}`);
+    helpBoardItemsData[i].classList.remove(`${HELP_BOARD_ITEM_ACTIVE_HELP}`);
     helpBoardItemsData[i].setAttribute("data-activehelp", `0`);
   }
+}
+
+const poopupVisible = (text) => {
+  popupTitle.innerHTML = text
+  popup.classList.remove('hidden')
 }
